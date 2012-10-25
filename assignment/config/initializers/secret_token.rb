77e4f75c41ed1753476b0b64120b3bbab5dc74c1
Prabhat <prabhat@Prabhats-MacBook-Air.local>
2012-10-25 14:11:59 -0400

    created a new project called assignment

---
+++ assignment/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Assignment::Application.config.secret_token = '6f3e353efec4c5e96744eb4e080eb4eebee52e0a805c744ebbf0ba0010015d5ea597ef1e01d3a5aaf233ef8411aa808e56d0e70c20c8ddc62a6ca1c1809f68a7'
